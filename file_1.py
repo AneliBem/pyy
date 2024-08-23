94bd25ba66fb1fed8835ff63efd9c6fc38ee975e
AneliBem <tolya.bem@gmail.com>
2024-08-24 01:46:35 +0300

    first commit

--- file_1.py
+++ file_1.py
@@ -1,6 +1,11 @@
-num = 5
-print("Hello World", num)
+print("Hello World")
 nv = 5 - 7 * 9
 print(nv)
 nb = 25 ** 6
 print(nb)
+if nv < 0:
+    print("negative")
+elif nv == 0:
+    print("Zero")
+elif nv > 0:
+    print("Positive")
