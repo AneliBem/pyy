d0c6edfe8f706b0cada2abe9e8663ef7d52c1c59
AneliBem <tolya.bem@gmail.com>
2024-09-09 22:36:46 +0300

    first commit

--- file_1.py
+++ file_1.py
@@ -1,40 +1,9 @@
-print("Hello World")
+def fib(n):
 
-nv = 5 - 7 * 9
-print(nv)
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
 
-nb = 25 ** 6
-print(nb)
-
-if nv < 0:
-    print("negative")
-elif nv == 0:
-    print("Zero")
-elif nv > 0:
-    print("Positive")
-for i in range(3):
-    print(i, end=" ")
-
-class Point:
-    __match_args__ = ('x', 'y')
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-def where_is(point):
-    match point:
-        case Point(x=0, y=0):
-            print("Origin")
-        case Point(x=0, y=y):
-            print(f"Y={y}")
-        case Point(x=x, y=0):
-            print(f"X={x}")
-        case Point():
-            print("Somewhere else")
-        case _:
-            print("Not a point")
-
-where_is(Point(0, 0))
-where_is(Point(0, 3))
-where_is(Point(4, 0))
-where_is(Point(5, 7))
+fib(5000)
