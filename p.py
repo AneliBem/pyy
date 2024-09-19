755f5767dcb75bdb1d178c5c4b50c4a4492b3213
AneliBem <tolya.bem@gmail.com>
2024-09-19 15:26:57 +0300

    first commit

--- p.py
+++ p.py
@@ -5,3 +5,17 @@ def f(arg=i):
 
 i = 6
 f()
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        reply = input(prompt)
+        if reply in {'y', 'ye', 'yes'}:
+            return True
+        if reply in {'n', 'no', 'nop', 'nope'}:
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+ask_ok('Ви дійсно хочете вийти?')
