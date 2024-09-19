2cfe024fc923525f1c46d738988e64e60dec8004
AneliBem <tolya.bem@gmail.com>
2024-09-19 15:33:22 +0300

    first commit

--- file_1.py
+++ file_1.py
@@ -19,3 +19,27 @@ num1 = float(input("a="))
 num2 = float(input("b="))
 
 add_numbers(num1, num2)
+
+
+
+i = 5
+
+def f(arg=i):
+    print(arg)
+
+i = 6
+f()
+
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        reply = input(prompt)
+        if reply in {'y', 'ye', 'yes'}:
+            return True
+        if reply in {'n', 'no', 'nop', 'nope'}:
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+ask_ok('Ви дійсно хочете вийти?')
