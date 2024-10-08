bf8647d09510fb3d287bec9d816af00cad974f81
AneliBem <tolya.bem@gmail.com>
2024-10-08 19:35:58 +0300

    first commit

--- file_1.py
+++ file_1.py
@@ -1,3 +1,4 @@
+from math import pi
 def fib(n):
 
     a, b = 0, 1
@@ -14,3 +15,34 @@ for x in range(10):
 
 squares
 print(squares)
+
+combs = []
+for x in [1,2,3]:
+    for y in [3,1,4]:
+        if x != y:
+            combs.append((x, y))
+
+combs
+print(combs)
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+print([str(round(pi, i)) for i in range(1, 6)])#1
+
+print([[row[i] for row in matrix] for i in range(4)]) #2
+
+transposed = []
+for i in range(4):
+    # the following 3 lines implement the nested listcomp
+    transposed_row = []
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+print(transposed) #3
+
+print(list(zip(*matrix)))#4
