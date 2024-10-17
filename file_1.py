2bbf8d4a63c3a7e74e5b815d7cbdcf4f1f959c1a
AneliBem <tolya.bem@gmail.com>
2024-10-17 19:05:50 +0300

    fetch commit

--- file_1.py
+++ file_1.py
@@ -77,3 +77,26 @@ print(a | b)                              # всі букви a i b
 print(a & b)                              # однакові
 
 print(a ^ b)                              # неоднакові
+
+
+tel = {'jack': 4098, 'sape': 4139}
+tel['guido'] = 4127
+print(tel)
+
+tel['jack']
+
+del tel['sape']
+tel['irv'] = 4127
+print(tel)
+
+print(list(tel))
+
+print(sorted(tel))
+
+print('guido' in tel)
+
+print('jack' not in tel)
+
+dic = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+
+print(dic)
